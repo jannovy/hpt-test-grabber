d7f3c9446be6ca0c7d4172d228b00160554fae22
Jan Novy <jan.novy@email.cz>
2021-05-21 15:37:59 +0200

    Fix - useless require

--- run.php
+++ run.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/vendor/simplehtmldom/simplehtmldom/simple_html_dom.php';
 
 use HPT\CZC\Grabber;
 use HPT\CZC\Output;
